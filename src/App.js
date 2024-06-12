96cfecb76e04fc3bac749300a9b010265a6dba16
Poonam Verma <poonam.verma02@nagarro.com>
2024-06-12 18:21:28 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-black">
+       <h2 className="text-white">Netflix gpt</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
